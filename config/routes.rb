8f18efea15ec10987716cca9446e7b62e77d9545
NadimRai <rai_nadim@yahoo.com>
2017-08-02 23:43:48 -0700

    created Photo route within Room route

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
       get 'amenities'
       get 'location'
     end
-   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :photos, only: [:create, :destroy]
+   end
 end
