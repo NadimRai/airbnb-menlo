e180da71ad2cfa780a739ad689c383d24aa19ad0
NadimRai <rai_nadim@yahoo.com>
2017-08-13 02:27:32 -0700

    ability to search with condition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,24 +4,26 @@ class PagesController < ApplicationController
   end
 
   def search
+    #STEP 1
   	if params[:search].present? && params[:search].strip != ""
   		session[:loc_search] = params[:search]
   	end
 
-  	arrResult = Array.new 
-
+    #STEP 2
   	if session[:loc_search] && session[:loc_search] != ""
   		@rooms_address = Room.where(active: true).near(session[:loc_search],5, order: "distance")
   	else
   		@rooms_address = Room.where(active: true).all 
   	end
 
+    #STEP3
   	@search = @rooms_address.ransack(params[:q])
   	@rooms = @search.result 
 
   	@arrRooms = @rooms.to_a 
 
-  	if (params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
+  	#STEP 4
+    if (params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
 
   		start_date = Date.parse(params[:start_date])
   		end_date = Date.parse(params[:end_date])
@@ -37,7 +39,7 @@ class PagesController < ApplicationController
   			).limit(1)
 
   			if not_available.lenght > 0
-  				@arrRooms.delete
+  				@arrRooms.delete(room)
   			end
   		end
   	end
