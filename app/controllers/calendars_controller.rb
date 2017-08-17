ca4c158b961b80b5000cd524d86b8b3530791779
NadimRai <rai_nadim@yahoo.com>
2017-08-18 00:04:13 +0800

    updated host calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,25 +1,36 @@
 class CalendarsController < ApplicationController
-	before_action: authenticate_user!
+	before_action :authenticate_user!
+	include ApplicationHelper
 
 	def host
-		@rooms = current_user.rooms
+    @rooms = current_user.rooms
 
-		params[:start_date] ||= Date.current.to_s
-		params[:room_id] ||= @rooms[0] ? @rooms[0].id : nil 
+    params[:start_date] ||= Date.current.to_s
+    params[:room_id] ||= @rooms[0] ? @rooms[0].id : nil
 
-		if params[:room_id]
-			@room = Room.find(params[:id])
-			start_date = Date.parse(params[:start_date])
+    if params[:q].present?
+      params[:start_date] = params[:q][:start_date]
+      params[:room_id] = params[:q][:room_id]
+    end
 
-			first_of_month = (start_date - 1.months).beginning_of_month
-			end_of_month = (start_date + 1.months).end_of_month
+    @search = Reservation.ransack(params[:q])
 
-			@events = @rooms.reservations.join(:user)
-							.select('reservations.*, users.full_name, users.image, users.email, users.uid')
-							.where('(start_date BETWEEN ? AND ?) AND status <> ?', first_of_month, end_of_month, 2)
-		else
-			@room = nil
-			@events = []
-		end
-	end
+    if params[:room_id]
+      @room = Room.find(params[:room_id])
+      start_date = Date.parse(params[:start_date])
+
+      first_of_month = (start_date - 1.months).beginning_of_month # => Jun 1
+      end_of_month = (start_date + 1.months).end_of_month # => Aug 31
+
+      @events = @room.reservations.joins(:user)
+                      .select('reservations.*, users.fullname, users.image, users.email, users.uid')
+                      .where('(start_date BETWEEN ? AND ?) AND status <> ?', first_of_month, end_of_month, 2)
+      @events.each{ |e| e.image = avatar_url(e) }
+      
+    else
+      @room = nil
+      @events = []
+      @days = []
+    end
+  end
 end
