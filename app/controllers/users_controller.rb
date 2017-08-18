0ae818883c1030ffabdece78b0dff85f51060d92
NadimRai <rai_nadim@yahoo.com>
2017-08-19 00:56:10 +0800

    improve payment form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,78 +1,73 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, except: [:show]
-	def show
-		@user = User.find(params[:id])
-		@rooms = @user.rooms
+  before_action :authenticate_user!, except: [:show]
 
-		#display all the guest reviews to host(if this user is a host)
-		@guest_reviews = Review.where(type: "GuestReview", host_id: @user.id)
+  def show
+    @user = User.find(params[:id])
+    @rooms = @user.rooms
 
-		#display all the host reviews to host(if this user is a guest)
-		@host_reviews = Review.where(type: "HostReview", guest_id: @user.id)
-	end
+    # Display all the guest reviews to host (if this user is a host)
+    @guest_reviews = Review.where(type: "GuestReview", host_id: @user.id)
 
-	def update_phone_number
-		current_user.update_attributes(user_params)
-		current_user.generate_pin 
-		current_user.send_pin
+    # Display all the host reviews to host (if this user is a guest)
+    @host_reviews = Review.where(type: "HostReview", guest_id: @user.id)
+  end
 
-		redirect_to edit_user_registration_path, notice: "Saved..."
-	rescue Exception => e 
-		redirect_to edit_user_registration_path, alert: "#{e.message}"
-	end
+  def update_phone_number
+    current_user.update_attributes(user_params)
+    current_user.generate_pin
+    current_user.send_pin
 
-	def verify_phone_number
-		current_user.verify_pin(params[:user][:pin])
+    redirect_to edit_user_registration_path, notice: "Saved..."
+  rescue Exception => e
+    redirect_to edit_user_registration_path, alert: "#{e.message}"
+  end
 
-		if current_user.phone_verified
-			flash[:notice] = "Your phone number is verified"
-		else
-			flash[:alert] = "Cannot verify your phone number"
-		end
+  def verify_phone_number
+    current_user.verify_pin(params[:user][:pin])
 
-		redirect_to edit_user_registration_path
+    if current_user.phone_verified
+      flash[:notice] = "Your phone number is verified."
+    else
+      flash[:alert] = "Cannot verify your phone number."
+    end
 
-		rescue Exception => e 
-		redirect_to edit_user_registration_path, alert: "#{e.message}"
+    redirect_to edit_user_registration_path
 
-	end
+  rescue Exception => e
+    redirect_to edit_user_registration_path, alert: "#{e.message}"
+  end
 
-	def payment
-	end
+  def payment
+  end
 
-	def add_card
-		if current_user.stripe_id.blank?
-			customer = Stripe::Customer.create(
-				email: current_user.email
-			)
-			current_user.stripe_id = customer.id 
-			current_user.save 
-		else
-			customer = Stripe::Customer.retrieve(current_user.stripe_id)
-		end
+  
 
-		#Add Credit Card to Stripe 
-		month, year = params[:expiry].split(/ \/ /)
-		new_token = Stripe::Token.create(:card => {
-			:number => params[:number],
-			:exp_month => month,
-			:exp_year => year,
-			:cvc => params[:cvc]
-		})
-		customer.sources.create(source: new_token.id)
+  def add_card
+    if current_user.stripe_id.blank?
+      customer = Stripe::Customer.create(
+        email: current_user.email
+      )
+      current_user.stripe_id = customer.id
+      current_user.save
 
-		flash[:notice] = "Your card is saved."
-		redirect_to payment_method_path
-	rescue Stripe::CardError => e 
-		flash[:alert] = e.message
-		redirect_to payment_method_path
-	end
+      # Add Credit Card to Stripe
+      customer.sources.create(source: params[:stripeToken])
+    else
+      customer = Stripe::Customer.retrieve(current_user.stripe_id)
+      customer.source = params[:stripeToken]
+      customer.save
+    end
 
-	private
+    flash[:notice] = "Your card is saved."
+    redirect_to payment_method_path
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to payment_method_path
+  end
 
-	
+  private
 
-	def user_params
-		params.require(:user).permit(:phone_number, :pin)
-	end
+    def user_params
+      params.require(:user).permit(:phone_number, :pin)
+    end
 end
